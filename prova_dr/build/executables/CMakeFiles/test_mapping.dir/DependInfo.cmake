ceb376e350111a48ecaea253b64ed07679e557f3
emanuelecaruso <caruso.manu81@gmail.com>
2021-10-27 16:48:57 +0200

    first commit

---
+++ prova_dr/build/executables/CMakeFiles/test_mapping.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/camera.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/camera.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/dtam.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/dtam.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/environment.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/environment.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/image.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/image.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/mapper.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/mapper.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/tracker.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/tracker.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/src/utils.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/__/src/utils.cpp.o"
+  "/home/emanuele/Scrivania/myalg/prova_dr/executables/test_mapping.cpp" "/home/emanuele/Scrivania/myalg/prova_dr/build/executables/CMakeFiles/test_mapping.dir/test_mapping.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/usr/include/python3.6m"
+  "../src"
+  "../src_cuda"
+  "../libs"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ prova_dr/build/executables/CMakeFiles/test_mapping.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_mapping.dir/test_mapping.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/camera.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/image.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/environment.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/dtam.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/utils.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/mapper.cpp.o"
+  "CMakeFiles/test_mapping.dir/__/src/tracker.cpp.o"
+  "test_mapping.pdb"
+  "test_mapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
